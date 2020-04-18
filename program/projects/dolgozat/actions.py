e7744be493707b293e6369ba2fd228d919e91428
danielnagy97 <nagydaniel.z97@gmail.com>
2020-04-18 11:50:09 +0200

    jumping between slides

--- program/projects/dolgozat/actions.py
+++ program/projects/dolgozat/actions.py
@@ -6,23 +6,17 @@ def placeholder(eze, aza):
     pass
 
 
-def next_slide(controller, button_widget):
-    if button_widget._pushed:
-        controller.current_scene += 1
-        button_widget._pushed = False
-
-
-def prew_slide(controller, button_widget):
-    if button_widget._pushed:
-        controller.current_scene -= 1
-        button_widget._pushed = False
+def jump_to(controller, button_widget):
+    if button_widget.pushed:
+        controller.current_scene = int(button_widget.arg)
+        button_widget.pushed = False
 
 
 def frame_diff_show(controller, button_widget):
-    if button_widget._pushed:
+    if button_widget.pushed:
 
         canvas = controller.frame_diff.canvas.canvas
-        canvas = cv2.resize(canvas, button_widget._dimension, fx=0, fy=0,
+        canvas = cv2.resize(canvas, button_widget.dimension, fx=0, fy=0,
                             interpolation=cv2.INTER_CUBIC)
         results_canvas = cv2.cvtColor(canvas,
                                       cv2.COLOR_GRAY2BGR)
@@ -30,21 +24,21 @@ def frame_diff_show(controller, button_widget):
 
 
 def heatmap_show(controller, button_widget):
-    if button_widget._pushed:
+    if button_widget.pushed:
 
         canvas = controller.heat_map.map
-        canvas = cv2.resize(canvas, button_widget._dimension, fx=0, fy=0,
+        canvas = cv2.resize(canvas, button_widget.dimension, fx=0, fy=0,
                             interpolation=cv2.INTER_AREA)
 
         button_widget._image = canvas
 
 
 def vector_field_show(controller, button_widget):
-    if button_widget._pushed:
+    if button_widget.pushed:
         controller.view.show_vector_field(controller.grid, controller.swirl,
                                           controller._canvasses['vectorfield'])
         canvas = controller._canvasses['vectorfield'].canvas
-        canvas = cv2.resize(canvas, button_widget._dimension, fx=0, fy=0,
+        canvas = cv2.resize(canvas, button_widget.dimension, fx=0, fy=0,
                             interpolation=cv2.INTER_CUBIC)
         results_canvas = cv2.cvtColor(canvas,
                                       cv2.COLOR_GRAY2BGR)
@@ -52,10 +46,10 @@ def vector_field_show(controller, button_widget):
 
 
 def symbol_show(controller, button_widget):
-    if button_widget._pushed:
+    if button_widget.pushed:
 
         canvas = controller._ocr.predicted_gest
-        canvas = cv2.resize(canvas, button_widget._dimension, fx=0, fy=0,
+        canvas = cv2.resize(canvas, button_widget.dimension, fx=0, fy=0,
                             interpolation=cv2.INTER_AREA)
         results_canvas = cv2.cvtColor(canvas,
                                       cv2.COLOR_GRAY2BGR)
@@ -63,22 +57,22 @@ def symbol_show(controller, button_widget):
 
 
 def sweep_show(controller, button_widget):
-    if button_widget._pushed:
+    if button_widget.pushed:
         controller.view.show_result_plot(controller.grid,
                                          controller._canvasses['resultplot'])
 
         canvas = controller._canvasses['resultplot'].canvas
-        canvas = cv2.resize(canvas, button_widget._dimension, fx=0, fy=0,
+        canvas = cv2.resize(canvas, button_widget.dimension, fx=0, fy=0,
                             interpolation=cv2.INTER_CUBIC)
 
         button_widget._image = canvas
 
 
 def grab_show(controller, button_widget):
-    if button_widget._pushed:
+    if button_widget.pushed:
 
         canvas = controller._grab.grab_image
-        canvas = cv2.resize(canvas, button_widget._dimension, fx=0, fy=0,
+        canvas = cv2.resize(canvas, button_widget.dimension, fx=0, fy=0,
                             interpolation=cv2.INTER_AREA)
         results_canvas = cv2.cvtColor(canvas,
                                       cv2.COLOR_GRAY2BGR)
