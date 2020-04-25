8b4257da87893dbef7090f2b15668a67174677dd
danielnagy97 <nagydaniel.z97@gmail.com>
2020-04-25 18:33:47 +0200

    dolgozat demo images in progress

--- program/main.py
+++ program/main.py
@@ -8,7 +8,11 @@ if __name__ == "__main__":
     # print(sys.argv)
     # project_path = "./projects/dolgozat/"
     project_path = sys.argv[1]
-    demo = False
+    mode = sys.argv[2]
+    if mode.lower() in ['true', '1', 't', 'y', 'yes', 'igen']:
+        demo = True
+    else:
+        demo = False
 
     controller = Controller(project_path, demo)
     controller.main_loop()
