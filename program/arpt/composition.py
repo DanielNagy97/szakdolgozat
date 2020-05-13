1ddc805084638bad2338706ab4cf6c053b4147d5
danielnagy97 <nagydaniel.z97@gmail.com>
2020-05-13 15:29:24 +0200

    removed the unused 'draw_expand' function

--- program/arpt/composition.py
+++ program/arpt/composition.py
@@ -69,24 +69,3 @@ class Composition():
 
         video.frame[pos_y:pos_y+height,
                     pos_x:pos_x+width, :] = blended
-
-    @staticmethod
-    def draw_expand(expand, video):
-        """
-        Draw the expandeble widget.
-        :param widget: an expandable widget
-        :param video: the video stream
-        :return: None
-        """
-        pos_x, pos_y = np.uint16(expand.position)
-        width, height = np.uint16(expand.dimension)
-        act_h = np.uint16(expand.actual_height)
-
-        added_image = \
-            cv2.addWeighted(video.frame[pos_y:pos_y+act_h,
-                                        pos_x:pos_x+width, :],
-                            0, expand.image,
-                            1-0, 0)
-
-        video.frame[pos_y:pos_y+act_h,
-                    pos_x:pos_x+width, :] = added_image
